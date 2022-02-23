784367f8ecaa131314f70b7e69e38ceb99866d37
ZoeYou <zuoyouwk@gmail.com>
2022-02-23 19:06:36 +0100

    updata parser

--- Parser.py
+++ Parser.py
@@ -1,4 +1,5 @@
 from anytree import Node, RenderTree
+from collections import defaultdict
 import re
 import copy
 
@@ -17,50 +18,76 @@ class Parser:
         res_forest = {}
 
         # remove "not otherwise provided for" from title
-        title = re.sub(r"[ ,;]?not otherwise provided for[,; ]?", "", title)
+        title = re.sub(r"[ ,]?not otherwise provided for[, ]?", "", title)
 
         # 1. split by semicolon
         titles = title.split(";")
         for t in titles:
-            t = re.sub(r" ?in general$", "", t.strip(", ")) # remove "in general" at the end of the title
+            t = re.sub(r"[ ,]?in general$", "", t.strip(", ")).strip(", ") # remove "in general" at the end of the title
             res_forest[t] = Node(t)
 
         # 2. split by e.g.
             if "e.g." in t:
                 try:
                     eg_p, eg_c = t.split('e.g.')
-                    eg_p = re.sub(r" ?in general$", "", eg_p.strip(", "))
-                    eg_c_nodes = [eg_c]
-                except ValueError: # more than one "e.g." in the title
-                    eg_list = [eg.strip(", ") for eg in t.split('e.g.')]
-                    eg_p = re.sub(r" ?in general$", "", eg_list[0]) 
-                    
-                    second_eg = " ".join([eg_list[1], eg_list[2]]) if eg_list[2].split()[0] in self.prep_list or eg_list[1].split()[-1] in self.prep_list else "e.g.".join([eg_list[1], eg_list[2]])
-                    eg_c_nodes = [eg_list[1], second_eg]
-
-                res_forest[t] = Node(eg_p)     
-                t_children = []
-                for eg_c in eg_c_nodes:
-                    eg_c = re.sub(r" ?in general$", "", eg_c.strip(", "))
+                    eg_p = re.sub(r"[ ,]?in general$", "", eg_p.strip(", "))
+                    eg_c = re.sub(r"[ ,]?in general$", "", eg_c.strip(", "))
+                    res_forest[t] = Node(eg_p)
 
                     # if the first word of example or the final word of head is a preprosition, concatename their names
-                    if eg_c.split()[0] in self.prep_list or eg_p.split()[-1] in self.prep_list:   
-                        child_title = " ".join([eg_p, eg_c])
-                    else:
-                        child_title = eg_c
-                    t_children.append(Node(child_title))
-                res_forest[t].children = t_children
+                    try:
+                        if eg_c.split()[0] in self.prep_list or eg_p.split()[-1] in self.prep_list:   
+                            eg_c = " ".join([eg_p, eg_c])
+                        res_forest[t].children = [Node(eg_c)]
+                    except IndexError:
+                         continue   # child node no content
 
-                # for pre, fill, node in RenderTree(res_forest[t]):
-                #     print("%s%s" % (pre, node.name))
+                except ValueError: # more than one "e.g." in the title
+                    eg_list = [eg.strip(", ") for eg in t.split('e.g.')]
+                    eg_list = [re.sub(r"[ ,]?in general$", "", eg) for eg in eg_list]
+                    eg_p = eg_list[0]
+
+                    res_forest[t] = Node(eg_p)
+                    Node_list = []                    
+                    for i in range(1, len(eg_list))[::-1]:
+                        eg_c = eg_list[i] 
+                        # if the first word of example or the final word of head is a preprosition, concatename their names
+                        if eg_c.split()[0] in self.prep_list or eg_list[i-1].split()[-1] in self.prep_list:   
+                            eg_c = " ".join([eg_list[i-1], eg_c])                 
+                        node_2_add = Node(eg_c)
+                        try:
+                            node_2_add.children = [Node_list[-1]]
+                        except IndexError:
+                            pass
+                        Node_list.append(node_2_add)
+                    res_forest[t].children = [Node_list[-1]]
         return res_forest
 
     def valide(self, p_node: str, c_nodes: list) -> list:
         """
-        check whether children nodes has the same title as parent title
+        1. check whether children nodes have the same title as parent title, if true remove the title in children nodes
+        2. check whether children nodes have repeated titles among each other, if true combine their sibling nodes 
         """
         p_name = p_node.name
-        return [c for c in c_nodes if c.name != p_name]
+        c_nodes = [c for c in c_nodes if c.name != p_name]
+
+        dict_name_siblings = defaultdict(list)  # values of dictinary are lists of Nodes
+        #nodes_name = [c.name for c in c_nodes]
+
+        for c in c_nodes:
+            dict_name_siblings[c.name].append(c)
+        flag = False
+        for k,v in dict_name_siblings.items():
+            if len(v) > 1:
+                flag = True
+        if flag:
+            for k, v in dict_name_siblings.items():
+                print(k)
+                print(v)
+                print("\n")
+            asdf
+        return c_nodes
+    
 
     def update_child_layer(self, p_node: Node, c_nodes: list, clean_p: bool) -> Node:
         """
@@ -70,16 +97,15 @@ class Parser:
             children_list = []
         else:
             children_list = list(p_node.children)   # original children nodes of the parent node
-
         for c in c_nodes:
             # if the first word of example or the final word of head is a preprosition, concatename their names
             try:
-                if c.name.split()[0] in self.prep_list or p_node.name.split()[-1] in self.prep_list:
+                if c.name.split()[0] in self.prep_list: # or p_node.name.split()[-1] in self.prep_list TODO
                     c.name = " ".join([p_node.name, c.name])
             except IndexError:
                 c_nodes.remove(c)
-
-        children_list.extend(c_nodes)
+        
+        children_list.extend(c_nodes)                   
         p_node.children = children_list
         return p_node
 
@@ -100,21 +126,21 @@ class Parser:
 
         # all nodes in post order 
         nodes_to_search = self.dfs_postorder([], root_node)
-
+        
         saved_nodes = {}
         top_layer = []
-
+          
         for node in nodes_to_search:
+            c_layer = list(self.split(node.name).values())  # returns a list of Nodes after splitting
+                
+            # if current node has children nodes already updated
+            if node.name + str(node.depth) in saved_nodes:
+                siblings = saved_nodes[node.name + str(node.depth)] # a list of children nodes of current node
+                for c in c_layer:
+                    c.children = self.valide(c, copy.deepcopy(c.children) + copy.deepcopy(siblings.children))
+
             p_node = copy.deepcopy(node.parent)
             if p_node:
-                c_layer = list(self.split(node.name).values())  # a list of Nodes
-                
-                # if child node has children nodes already updated
-                if node.name + str(node.depth) in saved_nodes:
-                    siblings = saved_nodes[node.name + str(node.depth)]
-                    for c in c_layer:
-                        c.children = self.valide(c, copy.deepcopy(siblings.children))
-
                 if p_node.name + str(p_node.depth) in saved_nodes:
                     new_p = False  
                     p_node = saved_nodes[p_node.name + str(p_node.depth)]
@@ -125,9 +151,8 @@ class Parser:
                 # update parent node in result dictionary
                 saved_nodes[p_node.name+str(p_node.depth)] = p_v
             else:
-                top_layer.append(saved_nodes[node.name + str(node.depth)])
+                top_layer.extend(c_layer)
+                #top_layer.append(saved_nodes[node.name + str(node.depth)])
             
         res_root.children = copy.deepcopy(top_layer)
-        for pre, fill, node in RenderTree(res_root):
-            print("%s%s" % (pre, node.name))
         return res_root
---
+++ parsing.py
@@ -0,0 +1,139 @@
+from anytree import Node, RenderTree
+from pathlib import Path
+from tqdm import tqdm
+import re, copy
+import pandas as pd
+import Parser
+
+
+TARGET_LEVEL = 6
+
+global dict_lvl
+dict_lvl = {1: -3, 3: -2, 4: -1, 6: 0}
+
+def convert_to_int(x):
+    lvl = x['lvl']
+    try:
+        return int(lvl)
+    except ValueError:
+        title = x['title']
+        if len(title) == 1:
+            return -3
+        elif len(title) == 3:
+            return -2
+        elif len(title) == 4:
+            return -1
+
+
+def clean_descr(description):
+    """
+    1. remove references from the description, such as (preserving A23B; obtaining protein compositions for foodstuffs A23J1/00;)
+    2. remove '{' and '}'
+    3. remove i.e. ***
+    """
+    description = description.replace('{', '').replace('}','')
+    description = re.sub(r'\ ?\([\w\W]*([A-Z]{1}[0-9]{2}[A-Z]{1}[0-9]*[\/]*[0-9]*)*[\w\W]*\)', '', description)
+    description = re.sub(r'[ ,]*i\.e\..*', '', description)
+    return description.lower()
+
+
+def has_cpc(description):
+    """
+    4. check whether a desciption has CPC code inside
+    """
+    cpc_pattern = r'[A-Za-z]{1}[0-9]{2}[A-Za-z]{0,1}[0-9]*[\/]*[0-9]*'
+    match = re.search(cpc_pattern, description)
+    if match:
+        return True
+    else:
+        return False
+
+def next_same_lvl_index(subdf,lvl):
+    lvl_df = subdf[subdf['lvl'] == lvl]
+    idx_df = lvl_df.index
+    res_index = idx_df[1] - 1
+    return res_index
+
+def rm_cpctitle_with_subtree(dataframe):
+    """
+    remove those titles with codes in their description and its subtree titles
+    """
+    # get indices of description 
+    dataframe = dataframe.reset_index(drop=True)
+    id_hascpc = dataframe[dataframe['description'].apply(has_cpc)].index
+    
+    idx_to_drop = []
+    for i in id_hascpc:
+        l = dataframe.iloc[i]
+        if l['lvl'] >= dataframe.iloc[i+1]['lvl']:
+            idx_to_drop.append(i)
+        else:
+            j = next_same_lvl_index(dataframe[i:], l['lvl'])
+            if i == j:
+                idx_to_drop.append(i)
+            elif j>i:
+                idx_to_drop.extend(range(i,j+1))
+            else:
+                raise ValueError("Problem with subtree index!")
+
+    dataframe = dataframe.drop(idx_to_drop)
+    return dataframe.reset_index(drop=True)
+
+
+def is_trash_title(description):
+    description = description.lower()
+    if "subject matter not" in description and "provided for in" in description:
+        return True
+    elif "covered by" in description and "subclass" in description and "other" in description:
+        return True
+    else:
+        return False
+
+def read_label_file(file_name, max_level=TARGET_LEVEL):
+    df = pd.read_csv(file_name, header=None, sep='\t', dtype=object, names=['title', 'lvl', 'description'])
+    df['lvl'] = df.apply(convert_to_int, axis=1)
+
+    if max_level in [1,3,4,6]:
+        df = df[df['lvl']<=dict_lvl[max_level]]
+
+    df['description'] = df['description'].apply(clean_descr)
+    df = df[~df['description'].apply(is_trash_title)]    # remove titles that are used as trash classes
+    df = rm_cpctitle_with_subtree(df)
+    return df.dropna().reset_index(drop=True)
+
+def find_father(df, child_lvl):
+    for i, row in df[::-1].iterrows():
+        if row.lvl + 1 == child_lvl:
+            return row.title
+
+def build_tree(df):
+    node_dict = {}
+    root_title = df.loc[0, 'title']
+    root_desc = df.loc[0, 'description']
+    node_dict[root_title] = Node(root_desc)
+
+    for i, row in df[1:].iterrows():
+        child_lvl = row.lvl
+        child_title = row.title
+        child_desc = row.description
+
+        father_title = find_father(df[:i], child_lvl)
+        node_dict[child_title] = Node(child_desc, parent = node_dict[father_title])
+    return node_dict[root_title]
+
+
+files_list = [str(f) for f in Path("cpc-titles").glob("*.txt")]
+output_path = Path("results")
+
+parser = Parser.Parser()
+for f in tqdm(files_list):
+    name = f.split("_")[0][-1]
+    df = read_label_file(f)
+    tree = build_tree(df)
+    res_root = parser.get_taxonomy(tree.root, name)
+
+    output_file = output_path / (name + '.txt')
+    with output_file.open('w') as out_f:
+        for pre, _, node in RenderTree(res_root):
+            out_f.write("%s%s" % (pre, node.name))
+            out_f.write("\n")
